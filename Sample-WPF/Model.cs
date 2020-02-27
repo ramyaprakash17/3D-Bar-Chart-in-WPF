20bb9c36da5601ee0abbe1c3b0f0d769e552bb1f
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-02-27 15:13:03 +0530

    Demo sample added

---
+++ Sample-WPF/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_WPF
+{
+    public class UserProfile
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public double NoOfUsers { get; set; }
+
+    }
+}
---
+++ Sample-WPF/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_WPF
+{
+    public class UsersViewModel
+    {
+        public UsersViewModel()
+        {
+            this.UsersList = new ObservableCollection<UserProfile>();
+            DateTime date = DateTime.Today;
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 1000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 5000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 3000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 4000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 2000 });
+            UsersList.Add(new UserProfile { TimeStamp = date.AddHours(0.5), NoOfUsers = 1000 });
+        }
+
+        public ObservableCollection<UserProfile> UsersList
+        {
+            get; set;
+        }
+
+    }
+}
